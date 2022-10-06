483be60680c545ab1f4372ea8e25c1ee83b23aeb
Wiese39 <1007024@hr.nl>
2022-10-06 14:36:08 +0200

    Pages
    pagina's gemaakt

---
+++ ClientApp/src/pages/Knowledge.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Knowledge() {
+    return (
+        <div>
+            <h1>Knowledge</h1>
+        </div>
+    )
+}
--- ClientApp/src/routes.js
+++ ClientApp/src/routes.js
@@ -1,9 +1,14 @@
 import { HomePage } from './pages/HomePage';
 import { NotFound} from './pages/NotFound';
 import { LoginPage } from './pages/Login';
+import { Knowledge } from './pages/Knowledge';
+import { Admin } from './pages/Admin';
 
 export const routes = [
     { path: '/', name: 'homePage', component: <HomePage /> },
+    { path: '/login', name: 'loginPage', component: <LoginPage /> },
+    { path: '/knowledge', name: 'knowledgePage', component: <Knowledge /> },
+    { path: '/admin', name: 'adminPage', component: <Admin /> },
     { path: '/*', name: 'notFound', component: <NotFound /> },
-    { path: '/login', name: 'loginPage', component: <LoginPage /> }
+    
 ];
