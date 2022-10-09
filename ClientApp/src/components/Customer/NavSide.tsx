1e620e1bb2a76dd6d6530718dc5d3f3f6561908d
The0Danktor <94552546+The0Danktor@users.noreply.github.com>
2022-10-09 23:41:05 +0200

    dark mode switch maar bug dat die niet werkt

--- ClientApp/src/components/Customer/NavSide.tsx
+++ ClientApp/src/components/Customer/NavSide.tsx
@@ -1,18 +1,13 @@
-import React from 'react'
+import React from "react";
+import { DarkmodeSwitch } from "../Shared/DarkmodeSwitch";
 
-export  function NavSide() {
-    const root = document.getElementsByTagName("html")[0];
-    const toggleDarkMode = () => {
-      root.setAttribute("class", "dark");
-      localStorage.theme = "dark";
-    };
-    const toggleLightMode = () => {
-      root.removeAttribute("class");
-      localStorage.theme = "light";
-    };
+export function NavSide() {
+
   return (
-    <div className="absolute left-0 h-screen w-80 flex flex-col justify-center bg-white dark:bg-gray-900 border-r-2 border-gray-800">
-        
+    <div className="absolute left-0 h-screen w-80 flex flex-col items-end bg-white dark:bg-gray-900 border-r-2 border-gray-800">
+      <div className=" relative right-2 top-4"> 
+        <DarkmodeSwitch />
+      </div>
     </div>
-  )
+  );
 }
--- ClientApp/src/pages/TestPage.tsx
+++ ClientApp/src/pages/TestPage.tsx
@@ -1,12 +1,11 @@
 import { NavSide } from "../components/Customer/NavSide";
 import React from 'react'
-import { DarkmodeSwitch } from "../components/Shared/DarkmodeSwitch";
+
 
 export function TestPage() {
   return (
     <div className="bg-white dark:bg-gray-900 w-screen h-screen">
-      {/* <NavSide /> */}
-      <DarkmodeSwitch />
+      <NavSide />
     </div>
   )
 }
