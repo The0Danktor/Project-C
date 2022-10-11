bb9c0f6376d0e64f0689ac9795fe759a7b7fe9b7
The0Danktor <94552546+The0Danktor@users.noreply.github.com>
2022-10-11 14:08:27 +0200

    Update NavSide.tsx

--- ClientApp/src/components/Customer/NavSide.tsx
+++ ClientApp/src/components/Customer/NavSide.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { DarkmodeSwitch } from "../Shared/DarkmodeSwitch";
 
 export function NavSide() {
-
+  
   return (
     <div className="absolute left-0 h-screen w-80 flex flex-col bg-white dark:bg-gray-900 border-r-2 border-gray-200 dark:border-gray-800 transition duration-300" >
       <div className="flex justify-between w-full py-3 px-3 border-b-2 border-gray-200 dark:border-gray-800 transition duration-300"> 
@@ -10,11 +10,11 @@ export function NavSide() {
         <DarkmodeSwitch />
       </div>
       <div className="flex flex-col item">
-        <p className="dark:text-white mx-4 my-2">Home</p>
-        <p className="dark:text-white mx-4 my-2">Tickets</p>
-        <p className="dark:text-white mx-4 my-2">KnowledgeBase</p>
-        <p className="dark:text-white mx-4 my-2">Admin</p>
-        <p className="dark:text-white mx-4 my-2">Settings</p>
+        <p className="dark:text-white mx-4 my-2 transition duration-300">Home</p>
+        <p className="dark:text-white mx-4 my-2 transition duration-300">Tickets</p>
+        <p className="dark:text-white mx-4 my-2 transition duration-300">KnowledgeBase</p>
+        <p className="dark:text-white mx-4 my-2 transition duration-300">Admin</p>
+        <p className="dark:text-white mx-4 my-2 transition duration-300">Settings</p>
       </div>
     </div>
   );
