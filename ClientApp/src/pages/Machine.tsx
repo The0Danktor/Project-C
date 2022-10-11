7baeb4bad693f6661f54fd0efaca3b39c934b2e7
Ant0n8 <a.shi2012@hotmail.com>
2022-10-11 15:05:58 +0200

    Borders

--- ClientApp/src/pages/Machine.tsx
+++ ClientApp/src/pages/Machine.tsx
@@ -2,24 +2,26 @@ import React from 'react'
 
 export function Machine() {
   return (
-    <div className="mt-96">
-      <div className="font-bold ml-36">
-        <h1 className="font-bold">Photo Machine1</h1>
-        <h1 className="font-bold ml-48">Photo Machine2</h1>
-        <h1 className="font-bold ml-96">Photo Machine3</h1>
+    <div className="flex-cols border-blue-500 border-t-8 border-b-8 border-l-8 border-r-8">
+      <div className="flex flex-col border-blue-500 border-l-8 border-t-8 border-b-8 border-r-8 space-y-4 ml-4 mb-4 mt-4">
+        <div>
+          <button className="bg-blue-500 hover:bg-blue-800 text-white font-bold py-2 px-6 rounded-3xl">
+          <h1>Machine1</h1>
+          </button>
+        </div>
+
+        <div>
+          <button className="bg-blue-500 hover:bg-blue-800 text-white font-bold py-2 px-6 rounded-3xl">
+          <h1>Machine2</h1>
+          </button>
+        </div>
+
+        <div>
+          <button className="bg-blue-500 hover:bg-blue-800 text-white font-bold py-2 px-6 rounded-3xl">
+          <h1>Machine3</h1>
+          </button>
+        </div>
       </div>
-
-      <button className="bg-blue-500 hover:bg-blue-800 text-white font-bold py-2 px-6 rounded-3xl mt-20 ml-24">
-      <h1>Machine1</h1>
-      </button>
-      
-      <button className="bg-blue-500 hover:bg-blue-800 text-white font-bold py-2 px-6 rounded-3xl ml-24">
-      <h1>Machine2</h1>
-      </button>
-
-      <button className="bg-blue-500 hover:bg-blue-800 text-white font-bold py-2 px-6 rounded-3xl ml-24">
-      <h1>Machine3</h1>
-      </button>
     </div>
   )
 }
