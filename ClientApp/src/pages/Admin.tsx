d73e61f2620467a57cc0aa7c640e7afee20bc765
Wiese39 <1007024@hr.nl>
2022-10-11 17:42:29 +0200

    SideBar Done
    Morgen even kijken hoe de rest vd pagina eruit moet gaan zien

--- ClientApp/src/pages/Admin.tsx
+++ ClientApp/src/pages/Admin.tsx
@@ -2,101 +2,32 @@ import React, { useState } from "react";
 import { DarkmodeSwitch } from "../components/Shared/DarkmodeSwitch";
 import control from "../assets/control.png";
 import viscon from "../assets/viscon.png";
+import { BsSearch, BsChevronDown } from "react-icons/bs";
+import { RiDashboardFill, RiAccountCircleLine, RiListSettingsLine, RiErrorWarningLine, RiLogoutBoxLine, RiInboxArchiveFill } from "react-icons/ri";
+import { TbUsers } from "react-icons/tb";
 
 export function Admin() {
-  const search = (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke-width="1.5"
-      stroke="currentColor"
-      className="w-6 h-6"
-    >
-      <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
-      />
-    </svg>
-  );
-  const settings = (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke-width="1.5"
-      stroke="currentColor"
-      className="w-6 h-6"
-    >
-      <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.324.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 011.37.49l1.296 2.247a1.125 1.125 0 01-.26 1.431l-1.003.827c-.293.24-.438.613-.431.992a6.759 6.759 0 010 .255c-.007.378.138.75.43.99l1.005.828c.424.35.534.954.26 1.43l-1.298 2.247a1.125 1.125 0 01-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.57 6.57 0 01-.22.128c-.331.183-.581.495-.644.869l-.213 1.28c-.09.543-.56.941-1.11.941h-2.594c-.55 0-1.02-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 01-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 01-1.369-.49l-1.297-2.247a1.125 1.125 0 01.26-1.431l1.004-.827c.292-.24.437-.613.43-.992a6.932 6.932 0 010-.255c.007-.378-.138-.75-.43-.99l-1.004-.828a1.125 1.125 0 01-.26-1.43l1.297-2.247a1.125 1.125 0 011.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.087.22-.128.332-.183.582-.495.644-.869l.214-1.281z"
-      />
-      <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-      />
-    </svg>
-  );
-  const home = (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke-width="1.5"
-      stroke="currentColor"
-      className="w-6 h-6"
-    >
-      <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
-      />
-    </svg>
-  );
-  const userIcon = (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke-width="1.5"
-      stroke="currentColor"
-      className="w-6 h-6"
-    >
-      <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
-      />
-    </svg>
-  );
-  const logoutIcon = (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke-width="1.5"
-      stroke="currentColor"
-      className="w-6 h-6"
-    >
-      <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75"
-      />
-    </svg>
-  );
-
   const [open, setOpen] = useState(false);
+  const [submenuOpen, setSubmenuOpen] = useState(false);
   const Menus = [
-    { title: "Accounts", src: { userIcon }, gap: true },
-    { title: "Home", src: {home} },
-    { title: "Settings", src: {settings} },
-    { title: "Search", src: {search} },
-    { title: "Logout", src: { logoutIcon } },
+    {
+      title: "Dashboard" 
+    },
+    { title: "Account", spacing: true, icon: <RiAccountCircleLine /> },
+    { title: "Users", spacing: true, icon: <TbUsers /> },
+    {
+      title: "Problems",
+      icon: <RiErrorWarningLine />,
+      submenu: true,
+      submenuItems: [
+        { title: "Problem 1" },
+        { title: "Problem 2" },
+        { title: "Problem 3" },
+      ],
+    },
+    { title: "Inbox", icon: <RiInboxArchiveFill /> },
+    { title: "Settings", icon: <RiListSettingsLine /> },
+    { title: "Logout", icon: <RiLogoutBoxLine /> },
   ];
   return (
     <div className="flex">
@@ -116,36 +47,77 @@ export function Admin() {
           <img
             src={viscon}
             className={`cursor-pointer duration-500 ${
-              open && "rotate-[360deg] w-32"
+              open && "rotate-[360deg] w-20"
             }`}
           />
           <h1
-            className={`text-white origin-left font-medium text-xl duration-200 ${
+            className={`text-white origin-top font-medium text-xl duration-200 ${
               !open && "scale-0"
             }`}
           >
             Admin
           </h1>
         </div>
-        <ul className="pt-6">
-          {Menus.map((Menu, index) => (
-            <li
-              key={index}
-              className={`flex  rounded-md p-2 cursor-pointer hover:bg-light-white text-gray-300 text-sm items-center gap-x-4 
-                        ${Menu.gap ? "mt-9" : "mt-2"} ${
-                index === 0 && "bg-light-white"
-              } `}
-            >
-              <img src={`${Menu.src}`} className="w-6 h-6" />
-              <span className={`${!open && "hidden"} origin-left duration-200`}>
-                {Menu.title}
-              </span>
-            </li>
+        <div
+          className={`flex items-center rounded-md bg-light-white mt-6 ${
+            !open ? "px-2.5" : "px-4"
+          } py-2`}
+        >
+          <BsSearch
+            className={`text-white text-lg block float-left cursor-pointer ${
+              open && "mr-2"
+            }`}
+          />
+          <input
+            type={"search"}
+            placeholder="Search"
+            className={`text-base bg-transparent w-full text-white focus:outline-none ${
+              !open && "hidden"
+            }`}
+          />
+        </div>
+        <ul className="pt-2">
+          {Menus.map((menu, index) => (
+            <>
+              <li
+                key={index}
+                className={`text-gray-300 text-sm flex items-center gap-x-4 cursor-pointer p-2 hover:bg-white hover:text-black rounded-md ${
+                  menu.spacing ? "mt-9" : "mt-2"
+                }`}
+              >
+                <span className="text-2xl block float-left">
+                  {menu.icon ? menu.icon : <RiDashboardFill />}
+                </span>
+                <span
+                  className={`text-base font-medium flex-1 ${
+                    !open && "hidden"
+                  }`}
+                >
+                  {menu.title}
+                </span>
+                {menu.submenu && open && (
+                  <BsChevronDown
+                    className={`${submenuOpen && "rotate-180"}`}
+                    onClick={() => setSubmenuOpen(!submenuOpen)}
+                  />
+                )}
+              </li>
+
+              {menu.submenu && submenuOpen && open &&(
+                <ul className="ml-8">
+                  {menu.submenuItems.map((submenuItem, index) => (
+                    <li
+                      key={index}
+                      className="text-gray-300 text-sm flex items-center gap-x-4 cursor-pointer p-2 px-5 hover:bg-white hover:text-black rounded-md"
+                    >
+                      {submenuItem.title}
+                    </li>
+                  ))}
+                </ul>
+              )}
+            </>
           ))}
         </ul>
-      </div>
-      <div className="p-7 text-2x1 font-semibold flex-1 h-screen">
-        <h1>Admin</h1>
       </div>
     </div>
   );
