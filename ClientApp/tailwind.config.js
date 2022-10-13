c03890a0edbc5bb5e88313d73a2c76a685c159b1
siwaipang <sisi_pang@hotmail.nl>
2022-10-13 13:12:08 +0200

    replaced new files with the old ones

--- ClientApp/tailwind.config.js
+++ ClientApp/tailwind.config.js
@@ -3,12 +3,7 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   darkMode: 'class',
   theme: {
-    extend: {
-      colors: {
-        "light-blue": "#E5F2FF",
-        "dark-blue": "#1E3A8A",
-      }
-    },
+    extend: {},
   },
   plugins: [],
 };
