7cdbc52cc4945a52699ec4f42a9790d7f5423c1e
Wiese39 <1007024@hr.nl>
2022-10-13 14:46:08 +0200

    Header / Dingen

--- ClientApp/src/components/Shared/AdminLayout.tsx
+++ ClientApp/src/components/Shared/AdminLayout.tsx
@@ -1,16 +1,21 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import { AdminSidebar } from "./AdminSidebar";
+import { NavSide } from "./NavSide";
 import { Header } from "./Header";
 
 export function Layout() {
   return (
-    <div className="flex">
-      <AdminSidebar />
+    <div className="flex  bg-white dark:bg-gray-900 transition duration-300">
+      <NavSide />
       <div>
         <Header />
-        <div className="flex flex-col mt-10 mx-10 w-40 h-40 bg-neutral hover:bg-slate-100 shadow-lg rounded-lg">
-            
+        <div className="flex flex-row">
+          <div className="flex flex-row mt-10 mx-10 w-40 h-40 font-mono text-black dark:text-gray-400 dark:hover:text-white bg-white dark:bg-gray-900 hover:bg-slate-100 dark:hover:bg-gray-800 dark:shadow-gray-600 shadow-lg rounded-lg transition duration-300">
+            Number of Machines
+          </div>
+          <div className="flex flex-row mt-10 mx-10 h-40 w-40 font-mono bg-white text-black dark:hover:text-white dark:text-gray-400 dark:bg-gray-900 hover:bg-slate-100 dark:hover:bg-gray-800 dark:shadow-gray-600 shadow-lg rounded-lg transition duration-300">
+            Number of accounts
+          </div>
         </div>
       </div>
     </div>
--- ClientApp/src/components/Shared/Header.tsx
+++ ClientApp/src/components/Shared/Header.tsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 export function Header() {
   return (
-    <div className="flex flex-row static px-20 w-screen h-20 bg-dark-blue">
-        <div className="text-2xl font-semibold">Dashboard</div>
+    <div className="flex flex-col h-24 w-screen bg-white dark:bg-gray-900 border-b-2 border-gray-100 dark:border-gray-800 transition duration-300">
+        <div className="flex flex-row h-24 w-screen text-2xl items-center text-right font-medium dark:text-gray-400 text-black transition duration-300">
+            Logged in as: Admin
+        </div>
     </div>
   )
 }
