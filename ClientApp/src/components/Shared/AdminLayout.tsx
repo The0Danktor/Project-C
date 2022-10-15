a032b83ac5c28477ce727191d1f5fbd601981e60
Wiese39 <1007024@hr.nl>
2022-10-15 21:27:30 +0200

    styiling small fix
    KLeine styling fix mbt uitlijning

--- ClientApp/src/components/Shared/AdminLayout.tsx
+++ ClientApp/src/components/Shared/AdminLayout.tsx
@@ -9,11 +9,20 @@ export function Layout() {
       <NavSide />
       <div>
         <Header />
-        <div className="flex flex-row">
-          <div className="flex flex-row mt-10 mx-10 w-40 h-40 font-mono text-black dark:text-gray-400 dark:hover:text-white bg-white dark:bg-gray-900 hover:bg-slate-100 dark:hover:bg-gray-800 dark:shadow-gray-600 shadow-lg rounded-lg transition duration-300">
+        <div className="flex justify-center font-semibold text-4xl text-black dark:text-gray-400 ">
+          Client Admin Panel
+        </div>
+        <div className="flex font-semibold text-black dark:text-gray-400 transition duration-300">
+          <div className="flex flex-row mt-40 mx-10 w-60 h-40 justify-center font-semibold text-black dark:text-gray-400 dark:hover:text-white bg-white dark:bg-gray-900 hover:bg-slate-100 dark:hover:bg-gray-800 dark:shadow-gray-600 shadow-lg rounded-lg transition duration-300">
             Number of Machines
           </div>
-          <div className="flex flex-row mt-10 mx-10 h-40 w-40 font-mono bg-white text-black dark:hover:text-white dark:text-gray-400 dark:bg-gray-900 hover:bg-slate-100 dark:hover:bg-gray-800 dark:shadow-gray-600 shadow-lg rounded-lg transition duration-300">
+          <div className="flex flex-row mt-40 justify-center mx-10 w-60 h-40 font-semibold bg-white text-black dark:hover:text-white dark:text-gray-400 dark:bg-gray-900 hover:bg-slate-100 dark:hover:bg-gray-800 dark:shadow-gray-600 shadow-lg rounded-lg transition duration-300">
+            Number of accounts
+          </div>
+          <div className="flex flex-row justify-center mt-40 mx-10 w-60 h-40 font-semibold bg-white text-black dark:hover:text-white dark:text-gray-400 dark:bg-gray-900 hover:bg-slate-100 dark:hover:bg-gray-800 dark:shadow-gray-600 shadow-lg rounded-lg transition duration-300">
+            Number of accounts
+          </div>
+          <div className="flex flex-row justify-center mt-40 mx-10 w-60 h-40 font-semibold bg-white text-black dark:hover:text-white dark:text-gray-400 dark:bg-gray-900 hover:bg-slate-100 dark:hover:bg-gray-800 dark:shadow-gray-600 shadow-lg rounded-lg transition duration-300">
             Number of accounts
           </div>
         </div>
