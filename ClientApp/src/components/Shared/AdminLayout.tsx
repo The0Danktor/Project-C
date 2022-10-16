f673b313229f6cea69b3a8b183d3e0874183652f
The0Danktor <94552546+The0Danktor@users.noreply.github.com>
2022-10-16 10:32:12 +0200

    Update AdminLayout.tsx

--- ClientApp/src/components/Shared/AdminLayout.tsx
+++ ClientApp/src/components/Shared/AdminLayout.tsx
@@ -16,9 +16,9 @@ export function Layout() {
           <div className="flex flex-row mt-20 justify-center mx-10 w-60 h-40 font-semibold bg-white text-black dark:hover:text-white dark:text-gray-400 dark:bg-gray-900 hover:bg-slate-100 dark:hover:bg-gray-800 dark:shadow-gray-600 shadow-lg rounded-lg transition duration-300">
             Number of Accounts
           </div>
-          <div className="flex flex-row justify-center mt-20 mx-10 w-60 h-40 font-semibold bg-white text-black dark:hover:text-white dark:text-gray-400 dark:bg-gray-900 hover:bg-slate-100 dark:hover:bg-gray-800 dark:shadow-gray-600 shadow-lg rounded-lg transition duration-300">
-            Number of Problems
-            <h1 className="flex flex-col justify-center items-center font-semibold text-2xl text-black dark:text-gray-400">
+          <div className="flex flex-col items-center  mt-20 mx-10 w-60 h-40 font-semibold bg-white text-black dark:hover:text-white dark:text-gray-400 dark:bg-gray-900 hover:bg-slate-100 dark:hover:bg-gray-800 dark:shadow-gray-600 shadow-lg rounded-lg transition duration-300">
+            <p>Number of Problems</p>
+            <h1 className="m-12 font-semibold text-2xl text-black dark:text-gray-400">
               22
             </h1>
           </div>
