2da27bfca500d50dec466442d866d39be37c4b9f
Wiese39 <1007024@hr.nl>
2022-10-16 10:56:38 +0200

    deel 2 pagina indeling begonnen
    flex box gemaakt voor tweede deel

--- ClientApp/src/components/Shared/AdminLayout.tsx
+++ ClientApp/src/components/Shared/AdminLayout.tsx
@@ -10,20 +10,28 @@ export function Layout() {
       <div>
         <Header />
         <div className="flex font-semibold text-black dark:text-gray-400 transition duration-300">
-          <div className="flex flex-row mt-20 mx-10 w-60 h-40 justify-center font-semibold text-black dark:text-gray-400 dark:hover:text-white bg-white dark:bg-gray-900 hover:bg-slate-100 dark:hover:bg-gray-800 dark:shadow-gray-600 shadow-lg rounded-lg transition duration-300">
-            Number of Machines
+        <div className="flex flex-col items-center text-2xl mt-20 mx-10 w-60 h-40 font-semibold bg-white text-black dark:hover:text-white dark:text-gray-400 dark:bg-gray-900 hover:bg-slate-100 dark:hover:bg-gray-800 dark:shadow-gray-600 shadow-lg rounded-lg transition duration-300">
+            <p>Number of Accounts</p>
+            <h1 className="m-12 font-semibold text-3xl text-black dark:text-gray-400">
+              15
+            </h1>
           </div>
-          <div className="flex flex-row mt-20 justify-center mx-10 w-60 h-40 font-semibold bg-white text-black dark:hover:text-white dark:text-gray-400 dark:bg-gray-900 hover:bg-slate-100 dark:hover:bg-gray-800 dark:shadow-gray-600 shadow-lg rounded-lg transition duration-300">
-            Number of Accounts
+          <div className="flex flex-col items-center text-2xl mt-20 mx-10 w-60 h-40 font-semibold bg-white text-black dark:hover:text-white dark:text-gray-400 dark:bg-gray-900 hover:bg-slate-100 dark:hover:bg-gray-800 dark:shadow-gray-600 shadow-lg rounded-lg transition duration-300">
+            <p>Number of Machines</p>
+            <h1 className="m-12 font-semibold text-3xl text-black dark:text-gray-400">
+              222
+            </h1>
           </div>
-          <div className="flex flex-col items-center  mt-20 mx-10 w-60 h-40 font-semibold bg-white text-black dark:hover:text-white dark:text-gray-400 dark:bg-gray-900 hover:bg-slate-100 dark:hover:bg-gray-800 dark:shadow-gray-600 shadow-lg rounded-lg transition duration-300">
+          <div className="flex flex-col items-center  mt-20 mx-10 w-60 h-40 font-semibold bg-white text-2xl text-black dark:hover:text-white dark:text-gray-400 dark:bg-gray-900 hover:bg-slate-100 dark:hover:bg-gray-800 dark:shadow-gray-600 shadow-lg rounded-lg transition duration-300">
             <p>Number of Problems</p>
-            <h1 className="m-12 font-semibold text-2xl text-black dark:text-gray-400">
-              22
+            <h1 className="m-12 font-semibold text-3xl text-black dark:text-gray-400">
+              0!
             </h1>
           </div>
         </div>
+        <div className="flex flex-row border-b-2 w-screen bg-white dark:bg-gray-900 transition duration-300 h-screen"></div>
       </div>
+      
     </div>
   );
 }
--- ClientApp/src/components/Shared/Header.tsx
+++ ClientApp/src/components/Shared/Header.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
-
+import {UserIcon} from "@heroicons/react/24/outline";
 export function Header() {
   return (
     <div className="flex flex-col h-24 w-screen bg-white dark:bg-gray-900 border-b-2 border-gray-100 dark:border-gray-800 transition duration-300">
         <div className="flex flex-row h-24 w-screen text-2xl items-center justify-center font-medium dark:text-gray-400 text-black transition duration-300">
             Logged in as: Admin
+            
         </div>
-        <div className="flex flex-row h-20 text-right">profile</div>
+        <div className="flex transition duration-300 text-black dark:text-gray-400"><UserIcon className="flex w-10 h-10"/></div> {/* justify end toevoegen svp, nu niet ivm met uitlijnen beeldscherm ratio voor pagina xD */}
     </div>
   )
 }
