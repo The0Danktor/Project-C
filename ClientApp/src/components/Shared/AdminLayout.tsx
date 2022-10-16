40436f24a02ef18fa2a2afb094252b3cac8ebe49
Wiese39 <1007024@hr.nl>
2022-10-16 10:28:24 +0200

    xx

--- ClientApp/src/components/Shared/AdminLayout.tsx
+++ ClientApp/src/components/Shared/AdminLayout.tsx
@@ -5,25 +5,22 @@ import { Header } from "./Header";
 
 export function Layout() {
   return (
-    <div className="flex  bg-white dark:bg-gray-900 transition duration-300">
+    <div className="flex bg-white dark:bg-gray-900 transition duration-300">
       <NavSide />
       <div>
         <Header />
-        <div className="flex justify-center font-semibold text-4xl text-black dark:text-gray-400 ">
-          Client Admin Panel
-        </div>
         <div className="flex font-semibold text-black dark:text-gray-400 transition duration-300">
-          <div className="flex flex-row mt-40 mx-10 w-60 h-40 justify-center font-semibold text-black dark:text-gray-400 dark:hover:text-white bg-white dark:bg-gray-900 hover:bg-slate-100 dark:hover:bg-gray-800 dark:shadow-gray-600 shadow-lg rounded-lg transition duration-300">
+          <div className="flex flex-row mt-20 mx-10 w-60 h-40 justify-center font-semibold text-black dark:text-gray-400 dark:hover:text-white bg-white dark:bg-gray-900 hover:bg-slate-100 dark:hover:bg-gray-800 dark:shadow-gray-600 shadow-lg rounded-lg transition duration-300">
             Number of Machines
           </div>
-          <div className="flex flex-row mt-40 justify-center mx-10 w-60 h-40 font-semibold bg-white text-black dark:hover:text-white dark:text-gray-400 dark:bg-gray-900 hover:bg-slate-100 dark:hover:bg-gray-800 dark:shadow-gray-600 shadow-lg rounded-lg transition duration-300">
-            Number of accounts
+          <div className="flex flex-row mt-20 justify-center mx-10 w-60 h-40 font-semibold bg-white text-black dark:hover:text-white dark:text-gray-400 dark:bg-gray-900 hover:bg-slate-100 dark:hover:bg-gray-800 dark:shadow-gray-600 shadow-lg rounded-lg transition duration-300">
+            Number of Accounts
           </div>
-          <div className="flex flex-row justify-center mt-40 mx-10 w-60 h-40 font-semibold bg-white text-black dark:hover:text-white dark:text-gray-400 dark:bg-gray-900 hover:bg-slate-100 dark:hover:bg-gray-800 dark:shadow-gray-600 shadow-lg rounded-lg transition duration-300">
-            Number of accounts
-          </div>
-          <div className="flex flex-row justify-center mt-40 mx-10 w-60 h-40 font-semibold bg-white text-black dark:hover:text-white dark:text-gray-400 dark:bg-gray-900 hover:bg-slate-100 dark:hover:bg-gray-800 dark:shadow-gray-600 shadow-lg rounded-lg transition duration-300">
-            Number of accounts
+          <div className="flex flex-row justify-center mt-20 mx-10 w-60 h-40 font-semibold bg-white text-black dark:hover:text-white dark:text-gray-400 dark:bg-gray-900 hover:bg-slate-100 dark:hover:bg-gray-800 dark:shadow-gray-600 shadow-lg rounded-lg transition duration-300">
+            Number of Problems
+            <h1 className="flex flex-col justify-center items-center font-semibold text-2xl text-black dark:text-gray-400">
+              22
+            </h1>
           </div>
         </div>
       </div>
--- ClientApp/src/components/Shared/Header.tsx
+++ ClientApp/src/components/Shared/Header.tsx
@@ -3,9 +3,10 @@ import React from 'react'
 export function Header() {
   return (
     <div className="flex flex-col h-24 w-screen bg-white dark:bg-gray-900 border-b-2 border-gray-100 dark:border-gray-800 transition duration-300">
-        <div className="flex flex-row h-24 w-screen text-2xl items- font-medium dark:text-gray-400 text-black transition duration-300">
+        <div className="flex flex-row h-24 w-screen text-2xl items-center justify-center font-medium dark:text-gray-400 text-black transition duration-300">
             Logged in as: Admin
         </div>
+        <div className="flex flex-row h-20 text-right">profile</div>
     </div>
   )
 }
