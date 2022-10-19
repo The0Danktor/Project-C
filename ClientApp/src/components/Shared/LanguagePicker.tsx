c233b0c10e6a88bcff10099bb0819e82f26959f9
The0Danktor <94552546+The0Danktor@users.noreply.github.com>
2022-10-19 23:46:05 +0200

    Update LanguagePicker.tsx

--- ClientApp/src/components/Shared/LanguagePicker.tsx
+++ ClientApp/src/components/Shared/LanguagePicker.tsx
@@ -1,17 +1,20 @@
-import { useTranslation } from 'react-i18next';
+import { useTranslation } from "react-i18next";
 
 export function LanguagePicker() {
   const { i18n } = useTranslation();
 
   const changeLanguage = (lng: string) => {
     i18n.changeLanguage(lng);
-    localStorage.setItem('i18nextLng', lng);
+    localStorage.setItem("i18nextLng", lng);
   };
 
   return (
-    <select onChange={e => changeLanguage(e.target.value)} value={localStorage.getItem('i18nextLng')?.split('-')[0]}>
-      <option value='en'>English</option>
-      <option value='nl'>Nederlands</option>
+    <select
+      onChange={(e) => changeLanguage(e.target.value)}
+      value={localStorage.getItem("i18nextLng")?.split("-")[0]}
+    >
+      <option value="en">English</option>
+      <option value="nl">Nederlands</option>
     </select>
   );
 }
