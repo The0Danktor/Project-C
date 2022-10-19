e859d12e02be100c01b9c701c6726f4636e02283
The0Danktor <94552546+The0Danktor@users.noreply.github.com>
2022-10-20 00:07:56 +0200

    menu items

--- ClientApp/src/components/Customer/ProblemMenu.tsx
+++ ClientApp/src/components/Customer/ProblemMenu.tsx
@@ -1,14 +1,18 @@
 import React from "react";
+import { ProblemOption } from "./ProblemOption";
 
 //https://plainenglish.io/blog/how-to-implement-a-search-bar-in-react-js
 export function ProblemMenu() {
   return (
-    <div className="">
-      <div className="border-b-2 w-fit">
-        <p>Select the option that suits your problem the most</p>
-        <input type="text" />
+    <div className="border-2 dark:border-gray-800 m-10 w-full rounded-md transition duration-300">
+      <div className="border-b-2 dark:border-gray-800 transition duration-300 p-5">
+        <p className="text-black dark:text-gray-400 transition duration-300" >Select the option that suits your problem the most</p>
       </div>
-      <div></div>
+      <div>
+        <ProblemOption/>
+        <ProblemOption/>
+        <ProblemOption/>
+      </div>
     </div>
   );
 }
