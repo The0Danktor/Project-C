5e99ff6b4de84cc1bc2797185577e1f190be911a
The0Danktor <94552546+The0Danktor@users.noreply.github.com>
2022-10-30 18:30:25 +0100

    test

--- ClientApp/src/pages/TestPageProblems.tsx
+++ ClientApp/src/pages/TestPageProblems.tsx
@@ -2,13 +2,16 @@ import { NavSide } from "../components/Shared/NavSide";
 import React from "react";
 import { ProblemMenu } from "../components/Customer/ProblemMenu";
 import Header from "../components/Shared/Header";
+import { useParams } from "react-router-dom";
 
-export function TestPage() {
+export function TestPageProblems() {
+  const { Problem_Id } = useParams();
+
   return (
     <div className="bg-white dark:bg-gray-900 transition flex duration-300">
       <NavSide />
-      {/* <Header /> */}
-      {/* <ProblemMenu /> */}
+       {/* <Header /> */}
+        <ProblemMenu />
     </div>
   );
 }
