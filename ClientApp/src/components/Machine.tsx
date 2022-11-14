ab788ee66d79c3604f5fdbe3ccd6a4a53ff86983
Ant0n8 <a.shi2012@hotmail.com>
2022-11-14 23:17:12 +0100

    Styling

--- ClientApp/src/components/Machine.tsx
+++ ClientApp/src/components/Machine.tsx
@@ -3,17 +3,19 @@ import React from "react"
 type MachineProps = {
     machineName: string,
     tekenNumber: string,
+    type: string,
     onclick: () => void
 }
 
-export function Machine({machineName, tekenNumber, onclick}: MachineProps) {
+export function Machine({machineName, tekenNumber, type, onclick}: MachineProps) {
     return (
         <li key={machineName}>
             <button
                 onClick={onclick}
-                className="bg-blue-300 dark:bg-gray-800 text-black dark:text-gray-400 dark:hover:text-white hover:bg-blue-400 dark:hover:bg-gray-700 py-5 px-96 rounded-xl border-2 border-blue-400 dark:border-gray-700">
-                Machinename: {machineName} 
-                Tekennumber: {tekenNumber}
+                className="flex flex-col gap-1 items-center w-full bg-blue-300 dark:bg-gray-800 text-black dark:text-gray-400 dark:hover:text-white hover:bg-blue-400 dark:hover:bg-gray-700 py-5 px-96 rounded-xl border-2 border-blue-400 dark:border-gray-700">
+                    <div className="px-5 py-1 rounded-xl border-2 border-blue-400 dark:border-gray-700">Name: {machineName}</div>
+                    <div className="px-5 py-1 rounded-xl border-2 border-blue-400 dark:border-gray-700">Tekennumber: {tekenNumber}</div>
+                    <div className="px-5 py-1 rounded-xl border-2 border-blue-400 dark:border-gray-700">Type: {type}</div>
             </button>
         </li>
     )
