eb8b57364e212cfa279cc7d435e20112f17df237
Ant0n8 <a.shi2012@hotmail.com>
2022-11-14 13:13:12 +0100

    Update Machines.tsx

--- ClientApp/src/pages/Machines.tsx
+++ ClientApp/src/pages/Machines.tsx
@@ -21,15 +21,15 @@ export function Machines() {
           <input type="string" placeholder="Search" className="bg-gray-200 hover:bg-gray-300 dark:bg-gray-200 dark:hover:bg-white text-black caret-black py-5 px-96 rounded-full text-center"/>
           <ul className="flex flex-col gap-8 overflow-y-auto items-center border-2 border-gray-100 dark:border-gray-800 p-10 rounded-xl">
             <Machine machineName="Machine 1 " tekenNumber="1" onclick={() => buttonClick("/machineproblems")}/>
-            <Machine machineName="Machine 2 " tekenNumber="1" onclick={() => buttonClick("/machineproblems")}/>
-            <Machine machineName="Machine 3 " tekenNumber="1" onclick={() => buttonClick("/machineproblems")}/>
-            <Machine machineName="Machine 4 " tekenNumber="1" onclick={() => buttonClick("/machineproblems")}/>
-            <Machine machineName="Machine 5 " tekenNumber="1" onclick={() => buttonClick("/machineproblems")}/>
-            <Machine machineName="Machine 6 " tekenNumber="1" onclick={() => buttonClick("/machineproblems")}/>
-            <Machine machineName="Machine 7 " tekenNumber="1" onclick={() => buttonClick("/machineproblems")}/>
-            <Machine machineName="Machine 8 " tekenNumber="1" onclick={() => buttonClick("/machineproblems")}/>
-            <Machine machineName="Machine 9 " tekenNumber="1" onclick={() => buttonClick("/machineproblems")}/>
-            <Machine machineName="Machine 10 " tekenNumber="1" onclick={() => buttonClick("/machineproblems")}/>
+            <Machine machineName="Machine 2 " tekenNumber="2" onclick={() => buttonClick("/machineproblems")}/>
+            <Machine machineName="Machine 3 " tekenNumber="3" onclick={() => buttonClick("/machineproblems")}/>
+            <Machine machineName="Machine 4 " tekenNumber="4" onclick={() => buttonClick("/machineproblems")}/>
+            <Machine machineName="Machine 5 " tekenNumber="5" onclick={() => buttonClick("/machineproblems")}/>
+            <Machine machineName="Machine 6 " tekenNumber="6" onclick={() => buttonClick("/machineproblems")}/>
+            <Machine machineName="Machine 7 " tekenNumber="7" onclick={() => buttonClick("/machineproblems")}/>
+            <Machine machineName="Machine 8 " tekenNumber="8" onclick={() => buttonClick("/machineproblems")}/>
+            <Machine machineName="Machine 9 " tekenNumber="9" onclick={() => buttonClick("/machineproblems")}/>
+            <Machine machineName="Machine 10 " tekenNumber="10" onclick={() => buttonClick("/machineproblems")}/>
           </ul>
         </div>
       </div>
