a9245bb57e34916d208adda5325b087c8170be8d
siwaipang <sisi_pang@hotmail.nl>
2022-11-20 16:56:28 +0100

    made the add picture button

--- ClientApp/src/pages/Ticket.tsx
+++ ClientApp/src/pages/Ticket.tsx
@@ -12,28 +12,24 @@ export function TicketPage() {
   for (var i = 0; i < 20; i++) {
     reports.push(<Ticket />);
   }
-  function Redirect() {
-    
-  }
+  function Redirect() {}
   return (
     <div className="flex dark:bg-gray-900 transition duration-300">
       <div className="hidden md:flex">
         <NavSide />
       </div>
       <div className="grow flex flex-row flex-wrap justify-start content-start transition duration-300 dark:bg-gray-800 bg-gray-100 md:h-[89vh] md:m-10 md:rounded-xl md:overflow-auto">
-        <div className="grow w-full p-3">
+        <div className="grow w-full m-2 md:m-3">
           <strong className="text-2xl">All tickets</strong>
-          <Link to="../AddImage"><button className="border w-full md:w-40 border-gray-300 dark:border-gray-700 dark:hover:bg-gray-700 
-          hover:bg-gray-200 dark:text-gray-400 rounded-lg ml-3 mb-2 md:mb-[unset] py-2"
-          onClick={Redirect}
-          >
-            Add Report
-          </button></Link>
-
-          {/* <NavSideButton
-            title="Add Report"
-            page="../addImage"
-          /> */}
+          <Link to="../AddImage">
+            <button
+              className="border w-full md:w-40 border-gray-300 dark:border-gray-700 dark:hover:bg-gray-700 
+          hover:bg-gray-200 dark:text-gray-400 rounded-lg md:ml-3 py-2"
+              onClick={Redirect}
+            >
+              Add Report
+            </button>
+          </Link>
         </div>
 
         {reports}
--- ClientApp/src/pages/addImage.tsx
+++ ClientApp/src/pages/addImage.tsx
@@ -2,9 +2,7 @@ import React from "react";
 import { NavSide } from "../components/Shared/NavSide";
 
 export function AddImage() {
-  function displayImg() {
-    
-  }
+  function displayImg() {}
   return (
     <div className="flex dark:bg-gray-900 transition duration-300">
       <div className="hidden md:flex">
@@ -12,10 +10,17 @@ export function AddImage() {
       </div>
       <div className="grow flex flex-row flex-wrap justify-start content-start transition duration-300 dark:bg-gray-800 bg-gray-100 md:h-[89vh] md:m-10 md:rounded-xl md:overflow-auto">
         <div className="grow w-full p-3">
-        <strong className="text-2xl">Select images</strong>
+          <strong className="text-2xl">Select images</strong>
           <form>
-            <div className="">
+            <div className="relative overflow-hidden inline-block">
+              <button
+                className="border w-screen md:w-40 border-gray-300 dark:border-gray-700 dark:hover:bg-gray-700 
+          hover:bg-gray-200 dark:text-gray-400 rounded-lg ml-3 mb-2 md:mb-[unset] py-2 !cursor-pointer"
+              >
+                Browse files
+              </button>
               <input
+<<<<<<< Updated upstream
               type="file"
               id="file"
               accept="image/*, video/*"
@@ -27,8 +32,16 @@ export function AddImage() {
           >
             Browse files
           </button>
+=======
+                type="file"
+                id="file"
+                accept="image/*, video/*"
+                onChange={displayImg}
+                multiple
+                className="absolute top-0 right-0 opacity-0 text-3xl cursor-pointer w-[93%]"
+              />
+>>>>>>> Stashed changes
             </div>
-            
           </form>
           <div id="display_images"></div>
           <img src="" alt="" />
