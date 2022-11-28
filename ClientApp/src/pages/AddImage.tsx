348b6db44199f5173f60fb905b4dfe71ffe161c9
siwaipang <sisi_pang@hotmail.nl>
2022-11-28 15:08:59 +0100

    show multiple images and videos

--- ClientApp/src/pages/AddImage.tsx
+++ ClientApp/src/pages/AddImage.tsx
@@ -1,27 +1,55 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { ImageGallery } from "../components/Shared/ImageGallery";
 import { NavSide } from "../components/Shared/NavSide";
 
 export function AddImage() {
-  const [imagePreview, setImagePreview] = useState();
-  const [videoPreview, setVideoPreview] = useState();
-  function displayImg(e : any) {
-    // Reading New File (open file Picker Box)
-    const reader = new FileReader();
-    // Gettting Selected File (user can select multiple but we are choosing only one)
-    const selectedFile = e.currentTarget.files[0];
-    if (selectedFile) {
-      reader.readAsDataURL(selectedFile);
-    }
-    // As the File loaded then set the stage as per the file type
-    reader.onload = (readerEvent : any) => {
-      if (selectedFile.type.includes("image")) {
-        setImagePreview(readerEvent.target.result);
-        // allImages.push(readerEvent.target.result);
-      } else if (selectedFile.type.includes("video")) {
-        setVideoPreview(readerEvent.target.result);
-      }
-    };
+  // show images
+  const [images, setImages] = useState([] as any);
+  const [imageURLS, setImageURLs] = useState([]);
+  useEffect(() => {
+    if (images.length < 1 || images.length > 11) return;
+    const listImages: any = [];
+    images.map((image: any) => listImages.push(URL.createObjectURL(image)));
+    setImageURLs(listImages);
+  }, [images]);
+
+  // show videos
+  const [videos, setVideos] = useState([] as any);
+  const [videoURLS, setVideoURLs] = useState([]);
+
+  useEffect(() => {
+    if (videos.length < 1 || videos.length > 11) return;
+    const listVideos: any = [];
+    images.map((video: any) => listVideos.push(URL.createObjectURL(video)));
+    setVideoURLs(listVideos);
+  }, [videos]);
+
+  function displayImg(e: any) {
+    setImages([...e.target.files]);
+    setVideos([...e.target.files]);
+
+    // // const listImages: any[] = [];
+    // function displayImg(e: any) {
+    //   // Reading New File (open file Picker Box)
+    //   const reader = new FileReader();
+    //   // Gettting Selected File (user can select multiple but we are choosing only one)
+    //   var key = e.currentTarget.files;
+    //   const selectedImage = Object.keys(key).map((element: any) => {
+    //     // const selectedImage = e.currentTarget.files[0];
+    //     if (key[element]) {
+    //       reader.readAsDataURL(key[element]);
+    //     }
+    //     // As the File loaded then set the stage as per the file type
+    //     reader.onload = (readerEvent: any) => {
+    //       if (key[element].type.includes("image")) {
+    //         setImagePreview(readerEvent.target.result);
+    //       } else if (key[element].type.includes("video")) {
+    //         setVideoPreview(readerEvent.target.result);
+    //       }
+    //     };
+
+    //   });
+    // listImages.push(<img src={imagePreview} className="w-64" />);
   }
   return (
     <div className="flex dark:bg-gray-900 transition duration-300">
@@ -53,12 +81,46 @@ export function AddImage() {
               />
             </div>
           </form>
-          {/* <ImageGallery src={[allImages]} /> */}
 
-          <img src={imagePreview} className="h-64"/>
-          <video controls src={videoPreview} className="h-64 hidden"></video>
+          {imageURLS.map((imagePreview) => (
+            <img src={imagePreview} className="min-h-[40px] max-h-64 my-2" />
+          ))}
+
+          {videoURLS.map((videoPreview) => (
+            <video controls src={videoPreview} className="min-h-[40px] max-h-64"></video>
+          ))}
+          {/* <img src={imagePreview} className="min-h-[40px] max-h-64"/>
+          {videoPreview != null && <video controls src={videoPreview} className="min-h-[40px] max-h-64"></video>} */}
         </div>
       </div>
     </div>
   );
 }
+
+// import React, { useState, useEffect } from "react";
+
+// const AddImage = () => {
+//   const [images, setImages] = useState([] as any);
+//   const [imageURLS, setImageURLs] = useState([]);
+//   useEffect(() => {
+//     if (images.length < 1) return;
+//     const newImageUrls: any = [];
+//     images.forEach((image:any) => newImageUrls.push(URL.createObjectURL(image)));
+//     setImageURLs(newImageUrls);
+//   }, [images]);
+
+//   function onImageChange(e: any) {
+//     setImages([...e.target.files]);
+//   }
+
+//   return (
+//     <>
+//       <input type="file" multiple accept="image/*" onChange={onImageChange} />
+//       {imageURLS.map((imageSrc) => (
+//         <img src={imageSrc} alt="not fount" width={"250px"} />
+//       ))}
+//     </>
+//   );
+// };
+
+// export default AddImage;
