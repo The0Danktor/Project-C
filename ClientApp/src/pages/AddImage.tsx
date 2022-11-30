00f20a8e465deec3eb4e221e59450f8a3577d338
siwaipang <sisi_pang@hotmail.nl>
2022-11-30 02:36:21 +0100

    pages scrollable on phone

--- ClientApp/src/pages/AddImage.tsx
+++ ClientApp/src/pages/AddImage.tsx
@@ -56,14 +56,14 @@ export function AddImage() {
       <div className="hidden md:flex">
         <NavSide />
       </div>
-      <div className="grow flex flex-row flex-wrap justify-start content-start transition duration-300 dark:bg-gray-800 bg-gray-100 md:h-[89vh] md:m-10 md:rounded-xl md:overflow-auto">
+      <div className="container">
         <div className="grow w-full p-3">
           <strong className="text-2xl">Select images</strong>
           <form>
             <div className="relative overflow-hidden ">
               <label htmlFor="file" className="cursor-pointer">
                 <p
-                  className="border w-screen md:w-40 border-gray-300 dark:border-gray-700 dark:hover:bg-gray-700 
+                  className="border md:w-40 border-gray-300 dark:border-gray-700 dark:hover:bg-gray-700 
                 hover:bg-gray-200 dark:text-gray-400 rounded-lg ml-3 mb-2 md:mb-[unset] py-2 text-center"
                 >
                   Browse files
