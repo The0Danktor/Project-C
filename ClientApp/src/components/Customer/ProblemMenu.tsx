d85bc8d6ad894c6a955f0d700ba885a8eaeb5f8f
Ant0n8 <a.shi2012@hotmail.com>
2022-12-08 13:56:49 +0100

    Test

--- ClientApp/src/components/Customer/ProblemMenu.tsx
+++ ClientApp/src/components/Customer/ProblemMenu.tsx
@@ -1,9 +1,34 @@
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import React from "react";
+import { ProblemAdd } from "./ProblemAdd";
+import { ProblemAddButton } from "./ProblemAddButton";
+import { ProblemList } from "./ProblemList";
 import { ProblemOption } from "./ProblemOption";
 
 //https://plainenglish.io/blog/how-to-implement-a-search-bar-in-react-js
 export function ProblemMenu() {
+
+  const list = [
+    <ProblemOption title="Product verkeerd op machine" machineType="Satelliet shuttle" solutions={["Fotocellen controleren","Eventueel product handmatig op juiste positie plaatsen"]} />,
+    <ProblemOption title="Product verkeerd op machine" machineType="Satelliet shuttle" solutions={["Fotocellen controleren","Eventueel product handmatig op juiste positie plaatsen"]} />,
+    <ProblemOption title="Product verkeerd op machine" machineType="Satelliet shuttle" solutions={["Fotocellen controleren","Eventueel product handmatig op juiste positie plaatsen"]} />,
+    <ProblemOption title="Product verkeerd op machine" machineType="Satelliet shuttle" solutions={["Fotocellen controleren","Eventueel product handmatig op juiste positie plaatsen"]} />,
+    <ProblemOption title="Product verkeerd op machine" machineType="Satelliet shuttle" solutions={["Fotocellen controleren","Eventueel product handmatig op juiste positie plaatsen"]} />,
+    <ProblemOption title="Product verkeerd op machine" machineType="Satelliet shuttle" solutions={["Fotocellen controleren","Eventueel product handmatig op juiste positie plaatsen"]} />,
+    <ProblemOption title="Product verkeerd op machine" machineType="Satelliet shuttle" solutions={["Fotocellen controleren","Eventueel product handmatig op juiste positie plaatsen"]} />,
+    <ProblemOption title="Product verkeerd op machine" machineType="Satelliet shuttle" solutions={["Fotocellen controleren","Eventueel product handmatig op juiste positie plaatsen"]} />,
+    <ProblemOption title="Product verkeerd op machine" machineType="Satelliet shuttle" solutions={["Fotocellen controleren","Eventueel product handmatig op juiste positie plaatsen"]} />,
+    <ProblemOption title="Product verkeerd op machine" machineType="Satelliet shuttle" solutions={["Fotocellen controleren","Eventueel product handmatig op juiste positie plaatsen"]} />,
+    <ProblemOption title="Product verkeerd op machine" machineType="Satelliet shuttle" solutions={["Fotocellen controleren","Eventueel product handmatig op juiste positie plaatsen"]} />,
+    <ProblemOption title="Product verkeerd op machine" machineType="Satelliet shuttle" solutions={["Fotocellen controleren","Eventueel product handmatig op juiste positie plaatsen"]} />,
+    <ProblemOption title="Product verkeerd op machine" machineType="Satelliet shuttle" solutions={["Fotocellen controleren","Eventueel product handmatig op juiste positie plaatsen"]} />,
+    <ProblemOption title="Product verkeerd op machine" machineType="Satelliet shuttle" solutions={["Fotocellen controleren","Eventueel product handmatig op juiste positie plaatsen"]} />,
+    <ProblemOption title="Product verkeerd op machine" machineType="Satelliet shuttle" solutions={["Fotocellen controleren","Eventueel product handmatig op juiste positie plaatsen"]} />,
+    <ProblemOption title="Product verkeerd op machine" machineType="Satelliet shuttle" solutions={["Fotocellen controleren","Eventueel product handmatig op juiste positie plaatsen"]} />,
+    <ProblemOption title="Product verkeerd op machine" machineType="Satelliet shuttle" solutions={["Fotocellen controleren","Eventueel product handmatig op juiste positie plaatsen"]} />,
+    <ProblemOption title="Product verkeerd op machine" machineType="Satelliet shuttle" solutions={["Fotocellen controleren","Eventueel product handmatig op juiste positie plaatsen"]} />
+  ]
+
   return (
     <div className="w-full p-10 overflow-y-scroll">
       <h1 className="text-black dark:text-gray-400 text-xl">
@@ -12,26 +37,9 @@ export function ProblemMenu() {
       {/*//TODO vergrootglas icon toevoegen */}
       <input type="text"  placeholder="Search" className="w-96 my-6 border-2 rounded-md focus:outline-none dark:bg-gray-900 dark:border-gray-800 px-3 py-2 dark:text-gray-400 dark:placeholder:text-gray-500 transition duration-300" />
       <div className="border-x-2 border-t-2 dark:border-gray-800  w-full rounded-md transition duration-300">
-        <ProblemOption title="Product verkeerd op machine" machineType="Satelliet shuttle" solutions={["Fotocellen controleren","Eventueel product handmatig op juiste positie plaatsen"]} />
-        <ProblemOption title="Product verkeerd op machine" machineType="Satelliet shuttle" solutions={["Fotocellen controleren","Eventueel product handmatig op juiste positie plaatsen"]} />
-        <ProblemOption title="Product verkeerd op machine" machineType="Satelliet shuttle" solutions={["Fotocellen controleren","Eventueel product handmatig op juiste positie plaatsen"]} />
-        <ProblemOption title="Product verkeerd op machine" machineType="Satelliet shuttle" solutions={["Fotocellen controleren","Eventueel product handmatig op juiste positie plaatsen"]} />
-        <ProblemOption title="Product verkeerd op machine" machineType="Satelliet shuttle" solutions={["Fotocellen controleren","Eventueel product handmatig op juiste positie plaatsen"]} />
-        <ProblemOption title="Product verkeerd op machine" machineType="Satelliet shuttle" solutions={["Fotocellen controleren","Eventueel product handmatig op juiste positie plaatsen"]} />
-        <ProblemOption title="Product verkeerd op machine" machineType="Satelliet shuttle" solutions={["Fotocellen controleren","Eventueel product handmatig op juiste positie plaatsen"]} />
-        <ProblemOption title="Product verkeerd op machine" machineType="Satelliet shuttle" solutions={["Fotocellen controleren","Eventueel product handmatig op juiste positie plaatsen"]} />
-        <ProblemOption title="Product verkeerd op machine" machineType="Satelliet shuttle" solutions={["Fotocellen controleren","Eventueel product handmatig op juiste positie plaatsen"]} />
-        <ProblemOption title="Product verkeerd op machine" machineType="Satelliet shuttle" solutions={["Fotocellen controleren","Eventueel product handmatig op juiste positie plaatsen"]} />
-        <ProblemOption title="Product verkeerd op machine" machineType="Satelliet shuttle" solutions={["Fotocellen controleren","Eventueel product handmatig op juiste positie plaatsen"]} />
-        <ProblemOption title="Product verkeerd op machine" machineType="Satelliet shuttle" solutions={["Fotocellen controleren","Eventueel product handmatig op juiste positie plaatsen"]} />
-        <ProblemOption title="Product verkeerd op machine" machineType="Satelliet shuttle" solutions={["Fotocellen controleren","Eventueel product handmatig op juiste positie plaatsen"]} />
-        <ProblemOption title="Product verkeerd op machine" machineType="Satelliet shuttle" solutions={["Fotocellen controleren","Eventueel product handmatig op juiste positie plaatsen"]} />
-        <ProblemOption title="Product verkeerd op machine" machineType="Satelliet shuttle" solutions={["Fotocellen controleren","Eventueel product handmatig op juiste positie plaatsen"]} />
-        <ProblemOption title="Product verkeerd op machine" machineType="Satelliet shuttle" solutions={["Fotocellen controleren","Eventueel product handmatig op juiste positie plaatsen"]} />
-        <ProblemOption title="Product verkeerd op machine" machineType="Satelliet shuttle" solutions={["Fotocellen controleren","Eventueel product handmatig op juiste positie plaatsen"]} />
-        <ProblemOption title="Product verkeerd op machine" machineType="Satelliet shuttle" solutions={["Fotocellen controleren","Eventueel product handmatig op juiste positie plaatsen"]} />
-        <ProblemOption title="Product verkeerd op machine" machineType="Satelliet shuttle" solutions={["Fotocellen controleren","Eventueel product handmatig op juiste positie plaatsen"]} />
-        <ProblemOption title="Product verkeerd op machine" machineType="Satelliet shuttle" solutions={["Fotocellen controleren","Eventueel product handmatig op juiste positie plaatsen"]} />
+        <ProblemList/>
+        <ProblemAddButton onclick={() => <ProblemAdd titleInput="Title" machineTypeInput="MachineType" solutionsInput={["solutions"]} list={list}/>} />
+        {/* <ProblemList/> */}
       </div>
     </div>
   );
