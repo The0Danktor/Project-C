05456ee589064cb7dfe110d16e6c9349672d896c
siwaipang <sisi_pang@hotmail.nl>
2022-12-20 02:56:49 +0100

    changed buttons layout in admin

--- ClientApp/src/components/Shared/AdminLayout.tsx
+++ ClientApp/src/components/Shared/AdminLayout.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import { NavSide } from "./NavSide";
 import { Header } from "./Header";
-import { Link } from "react-router-dom";
+import { ButtonAdmin } from "./Button";
 import {
   Chart as ChartJS,
   LinearScale,
@@ -103,93 +103,152 @@ export function Layout() {
     return items;
   }
   return (
-    <div className="flex bg-white dark:bg-gray-900 transition duration-300">
+    <div className="flex dark:bg-gray-900 transition duration-300">
       <NavSide />
-      <div>
+      <div className="flex flex-col grow">
         <Header />
-        <div className="flex flex-row flex-wrap w-screen font-semibold text-black dark:text-gray-400 transition duration-300">
-          <div className="flex flex-col items-center justify-center mt-20 mx-10 w-60 h-40 font-semibold bg-white text-2xl text-black dark:hover:text-white dark:text-gray-400 dark:bg-gray-900 hover:bg-slate-100 dark:hover:bg-gray-800 dark:shadow-gray-600 shadow-lg rounded-lg transition duration-300 cursor-pointer">
-            <Link to="accounts">
-              <p className="mt-2">Our Accounts</p>
-              <h1 className="m-12 font-semibold text-3xl text-black dark:text-gray-400">
-                bar
-              </h1>
-            </Link>
+        <div className="container">
+          <div className="flex flex-col sm:flex-row flex-wrap font-semibold text-black dark:text-gray-400 transition duration-300">
+          {/* button links */}
+          <ButtonAdmin linkTo="accounts" title="Our Accounts" bar="bar"/>
+          <ButtonAdmin linkTo="machines" title="Our Machines" bar="bar"/>
+          <ButtonAdmin linkTo="problems" title="History" bar="bar"/>
+          <ButtonAdmin linkTo="dev" title="placeholders" bar="bar"/>
+          {/* <ButtonAdmin linkTo="dev" title="placeholders" bar="bar"/> */}
           </div>
-          <div className="flex flex-col items-center justify-center mt-20 mx-10 w-60 h-40 font-semibold bg-white text-2xl text-black dark:hover:text-white dark:text-gray-400 dark:bg-gray-900 hover:bg-slate-100 dark:hover:bg-gray-800 dark:shadow-gray-600 shadow-lg rounded-lg transition duration-300 cursor-pointer">
-            <Link to="machines">
-              <p className="mt-2">Our Machines</p>
-              <h1 className="m-12 font-semibold text-3xl text-black dark:text-gray-400">
-                bar
-              </h1>
-            </Link>
-          </div>
-          <div className="flex flex-col items-center justify-center mt-20 mx-10 w-60 h-40 font-semibold bg-white text-2xl text-black dark:hover:text-white dark:text-gray-400 dark:bg-gray-900 hover:bg-slate-100 dark:hover:bg-gray-800 dark:shadow-gray-600 shadow-lg rounded-lg transition duration-300 cursor-pointer">
-            <Link to="problems">
-              <p className="mt-2 mx-4">History</p>
-              <h1 className="m-12 font-semibold text-3xl text-black dark:text-gray-400">
-                bar
-              </h1>
-            </Link>
-          </div>
-          <div className="flex flex-col items-center justify-center mt-20 mx-10 w-60 h-40 font-semibold bg-white text-2xl text-black dark:hover:text-white dark:text-gray-400 dark:bg-gray-900 hover:bg-slate-100 dark:hover:bg-gray-800 dark:shadow-gray-600 shadow-lg rounded-lg transition duration-300 cursor-pointer">
-            <Link to="dev">
-              <p>placeholders</p>
-              <h1 className="m-12 font-semibold text-3xl text-black dark:text-gray-400">
-                bar
-              </h1>
-            </Link>
-          </div>
-          <div className="flex flex-col items-center justify-center mt-20 mx-10 w-60 h-40 font-semibold bg-white text-2xl text-black dark:hover:text-white dark:text-gray-400 dark:bg-gray-900 hover:bg-slate-100 dark:hover:bg-gray-800 dark:shadow-gray-600 shadow-lg rounded-lg transition duration-300 cursor-pointer">
-            <Link to="dev">
-              <p>placeholder</p>
-              <h1 className="m-12 font-semibold text-3xl text-black dark:text-gray-400">
-                bar
-              </h1>
-            </Link>
-          </div>
-        </div>
-        <div className="flex w-full font-semibold text-black dark:text-gray-400 transition duration-300">
-          <div className="flex flex-col items-center mt-40 mx-10 w-1/6 h-1/3 border-4 border-gray-100 dark:border-gray-800 dark:shadow-gray-800 shadow-lg rounded-3xl">
-            <div className="flex flex-col justify-center items-center w-60 text-2xl mt-2 h-10 font-semibold bg-white text-black dark:hover:text-white dark:text-gray-400 border-2 border-gray-100 dark:border-gray-800 dark:bg-gray-900 hover:bg-slate-100 dark:hover:bg-gray-800 dark:shadow-gray-600 shadow-lg rounded-full transition duration-300">
-              <p>Recent Activity</p>
+
+          {/* recent activity */}
+          <div className="flex justify-between font-semibold w-full text-black dark:text-gray-400 transition duration-300">
+            <div className="flex flex-col items-center mt-4 mx-2 w-fit h-fit p-2 border border-gray-300 dark:border-gray-700 rounded-3xl">
+              <div className="flex flex-col justify-center items-center w-60 text-2xl mt-2 h-10 font-semibold bg-white text-black dark:hover:text-white dark:text-gray-400 border-2 border-gray-100 dark:border-gray-800 dark:bg-gray-900 hover:bg-slate-100 dark:hover:bg-gray-800 dark:shadow-gray-600 shadow-lg rounded-full transition duration-300">
+                <p>Recent Activity</p>
+              </div>
+              <div className="flex flex-col justify-center font-semibold text-2xl ">
+                {renderMenuItems()}
+              </div>
             </div>
-            <div className="flex flex-col justify-center font-semibold text-2xl ">
-              {renderMenuItems()}
+            <div className="flex flex-col items-center mt-4 mr-7 w-fit h-fit p-2 border border-gray-300 dark:border-gray-700 rounded-3xl">
+              <div className="flex flex-col justify-center items-center w-60 text-2xl mt-2 h-10 font-semibold bg-white text-black dark:hover:text-white dark:text-gray-400 border-2 border-gray-100 dark:border-gray-800 dark:bg-gray-900 hover:bg-slate-100 dark:hover:bg-gray-800 dark:shadow-gray-600 shadow-lg rounded-full transition duration-300">
+                <p>Recent Errors</p>
+              </div>
+              <div className="flex flex-col justify-center font-semibold text-2xl ">
+                {renderMenuItems()}
+              </div>
             </div>
+            {/* <div className="flex flex-col items-center mt-40 mx-10 w-1/6 h-1/3 border-4 border-gray-100 dark:border-gray-800 dark:shadow-gray-800 shadow-lg rounded-3xl">
+              <div className="flex flex-col justify-center items-center w-60 text-2xl mt-2 h-10 font-semibold bg-white text-black dark:hover:text-white dark:text-gray-400 border-2 border-gray-100 dark:border-gray-800 dark:bg-gray-900 hover:bg-slate-100 dark:hover:bg-gray-800 dark:shadow-gray-600 shadow-lg rounded-full transition duration-300">
+                <p>Wee nie</p>
+              </div>
+              <div className="flex flex-col justify-center font-semibold text-2xl ">
+                {renderMenuItems()}
+              </div>
+            </div>
+            <div className="flex flex-col items-center mt-40 mx-10 w-1/6 h-1/3 border-4 border-gray-100 dark:border-gray-800 dark:shadow-gray-800 shadow-lg rounded-3xl">
+              <div className="flex flex-col justify-center items-center w-60 text-2xl mt-2 h-10 font-semibold bg-white text-black dark:hover:text-white dark:text-gray-400 border-2 border-gray-100 dark:border-gray-800 dark:bg-gray-900 hover:bg-slate-100 dark:hover:bg-gray-800 dark:shadow-gray-600 shadow-lg rounded-full transition duration-300">
+                <p>Wee nie</p>
+              </div>
+              <div className="flex flex-col justify-center font-semibold text-2xl ">
+                {renderMenuItems()}
+              </div>
+            </div> */}
           </div>
-          <div className="flex flex-col items-center mt-40 mx-10 w-1/6 h-1/3 border-4 border-gray-100 dark:border-gray-800 dark:shadow-gray-800 shadow-lg rounded-3xl">
-            <div className="flex flex-col justify-center items-center w-60 text-2xl mt-2 h-10 font-semibold bg-white text-black dark:hover:text-white dark:text-gray-400 border-2 border-gray-100 dark:border-gray-800 dark:bg-gray-900 hover:bg-slate-100 dark:hover:bg-gray-800 dark:shadow-gray-600 shadow-lg rounded-full transition duration-300">
-              <p>Recent Errors</p>
-            </div>
-            <div className="flex flex-col justify-center font-semibold text-2xl ">
-              {renderMenuItems()}
-            </div>
-          </div>
-          <div className="flex flex-col items-center mt-40 mx-10 w-1/6 h-1/3 border-4 border-gray-100 dark:border-gray-800 dark:shadow-gray-800 shadow-lg rounded-3xl">
-            <div className="flex flex-col justify-center items-center w-60 text-2xl mt-2 h-10 font-semibold bg-white text-black dark:hover:text-white dark:text-gray-400 border-2 border-gray-100 dark:border-gray-800 dark:bg-gray-900 hover:bg-slate-100 dark:hover:bg-gray-800 dark:shadow-gray-600 shadow-lg rounded-full transition duration-300">
-              <p>Wee nie</p>
-            </div>
-            <div className="flex flex-col justify-center font-semibold text-2xl ">
-              {renderMenuItems()}
-            </div>
-          </div>
-          <div className="flex flex-col items-center mt-40 mx-10 w-1/6 h-1/3 border-4 border-gray-100 dark:border-gray-800 dark:shadow-gray-800 shadow-lg rounded-3xl">
-            <div className="flex flex-col justify-center items-center w-60 text-2xl mt-2 h-10 font-semibold bg-white text-black dark:hover:text-white dark:text-gray-400 border-2 border-gray-100 dark:border-gray-800 dark:bg-gray-900 hover:bg-slate-100 dark:hover:bg-gray-800 dark:shadow-gray-600 shadow-lg rounded-full transition duration-300">
-              <p>Wee nie</p>
-            </div>
-            <div className="flex flex-col justify-center font-semibold text-2xl ">
-              {renderMenuItems()}
-            </div>
-          </div>
-        </div>
-        {/* <div className="flex flex-row w-full mt-14 mx-10 dark:text-gray-400 items-start font-semibold text-2xl transition duration-300">
+          {/* <div className="flex flex-row w-full mt-14 mx-10 dark:text-gray-400 items-start font-semibold text-2xl transition duration-300">
           
           <div className="mt-2 w-1/2 h-3/4 items-start border-gray-100 dark:border-gray-800 border-2 rounded-2xl shadow-lg dark:shadow-gray-800"><Chart type="bar" data={data}/></div>
           
           <div className="mt-2 w-1/2 h-3/4 items-end border-gray-100 dark:border-gray-800 border-2 rounded-2xl shadow-lg dark:shadow-gray-800"><Chart type="bar" data={data}/></div>
         </div> */}
+        </div>
       </div>
     </div>
   );
 }
+
+// <div className="flex bg-white dark:bg-gray-900 transition duration-300">
+//   <NavSide />
+//   <div>
+//     <Header />
+//     <div className="flex font-semibold text-black dark:text-gray-400 transition duration-300">
+//       <div className="flex flex-col items-center justify-center mt-20 mx-10 w-60 h-40 font-semibold bg-white text-2xl text-black dark:hover:text-white dark:text-gray-400 dark:bg-gray-900 hover:bg-slate-100 dark:hover:bg-gray-800 dark:shadow-gray-600 shadow-lg rounded-lg transition duration-300 cursor-pointer">
+//         <Link to="accounts">
+//           <p className="mt-2">Our Accounts</p>
+//           <h1 className="m-12 font-semibold text-3xl text-black dark:text-gray-400">
+//             bar
+//           </h1>
+//         </Link>
+//       </div>
+//       <div className="flex flex-col items-center justify-center mt-20 mx-10 w-60 h-40 font-semibold bg-white text-2xl text-black dark:hover:text-white dark:text-gray-400 dark:bg-gray-900 hover:bg-slate-100 dark:hover:bg-gray-800 dark:shadow-gray-600 shadow-lg rounded-lg transition duration-300 cursor-pointer">
+//         <Link to="machines">
+//           <p className="mt-2">Our Machines</p>
+//           <h1 className="m-12 font-semibold text-3xl text-black dark:text-gray-400">
+//             bar
+//           </h1>
+//         </Link>
+//       </div>
+//       <div className="flex flex-col items-center justify-center mt-20 mx-10 w-60 h-40 font-semibold bg-white text-2xl text-black dark:hover:text-white dark:text-gray-400 dark:bg-gray-900 hover:bg-slate-100 dark:hover:bg-gray-800 dark:shadow-gray-600 shadow-lg rounded-lg transition duration-300 cursor-pointer">
+//         <Link to="problems">
+//           <p className="mt-2 mx-4">History</p>
+//           <h1 className="m-12 font-semibold text-3xl text-black dark:text-gray-400">
+//             bar
+//           </h1>
+//         </Link>
+//       </div>
+//       <div className="flex flex-col items-center justify-center mt-20 mx-10 w-60 h-40 font-semibold bg-white text-2xl text-black dark:hover:text-white dark:text-gray-400 dark:bg-gray-900 hover:bg-slate-100 dark:hover:bg-gray-800 dark:shadow-gray-600 shadow-lg rounded-lg transition duration-300 cursor-pointer">
+//         <Link to="dev">
+//           <p>placeholders</p>
+//           <h1 className="m-12 font-semibold text-3xl text-black dark:text-gray-400">
+//             bar
+//           </h1>
+//         </Link>
+//       </div>
+//       <div className="flex flex-col items-center justify-center mt-20 mx-10 w-60 h-40 font-semibold bg-white text-2xl text-black dark:hover:text-white dark:text-gray-400 dark:bg-gray-900 hover:bg-slate-100 dark:hover:bg-gray-800 dark:shadow-gray-600 shadow-lg rounded-lg transition duration-300 cursor-pointer">
+//         <Link to="dev">
+//           <p>placeholder</p>
+//           <h1 className="m-12 font-semibold text-3xl text-black dark:text-gray-400">
+//             bar
+//           </h1>
+//         </Link>
+//       </div>
+//     </div>
+//     <div className="flex w-full font-semibold text-black dark:text-gray-400 transition duration-300">
+//       <div className="flex flex-col items-center mt-40 mx-10 w-1/6 h-1/3 border-4 border-gray-100 dark:border-gray-800 dark:shadow-gray-800 shadow-lg rounded-3xl">
+//         <div className="flex flex-col justify-center items-center w-60 text-2xl mt-2 h-10 font-semibold bg-white text-black dark:hover:text-white dark:text-gray-400 border-2 border-gray-100 dark:border-gray-800 dark:bg-gray-900 hover:bg-slate-100 dark:hover:bg-gray-800 dark:shadow-gray-600 shadow-lg rounded-full transition duration-300">
+//           <p>Recent Activity</p>
+//         </div>
+//         <div className="flex flex-col justify-center font-semibold text-2xl ">
+//           {renderMenuItems()}
+//         </div>
+//       </div>
+//       <div className="flex flex-col items-center mt-40 mx-10 w-1/6 h-1/3 border-4 border-gray-100 dark:border-gray-800 dark:shadow-gray-800 shadow-lg rounded-3xl">
+//         <div className="flex flex-col justify-center items-center w-60 text-2xl mt-2 h-10 font-semibold bg-white text-black dark:hover:text-white dark:text-gray-400 border-2 border-gray-100 dark:border-gray-800 dark:bg-gray-900 hover:bg-slate-100 dark:hover:bg-gray-800 dark:shadow-gray-600 shadow-lg rounded-full transition duration-300">
+//           <p>Recent Errors</p>
+//         </div>
+//         <div className="flex flex-col justify-center font-semibold text-2xl ">
+//           {renderMenuItems()}
+//         </div>
+//       </div>
+//       <div className="flex flex-col items-center mt-40 mx-10 w-1/6 h-1/3 border-4 border-gray-100 dark:border-gray-800 dark:shadow-gray-800 shadow-lg rounded-3xl">
+//         <div className="flex flex-col justify-center items-center w-60 text-2xl mt-2 h-10 font-semibold bg-white text-black dark:hover:text-white dark:text-gray-400 border-2 border-gray-100 dark:border-gray-800 dark:bg-gray-900 hover:bg-slate-100 dark:hover:bg-gray-800 dark:shadow-gray-600 shadow-lg rounded-full transition duration-300">
+//           <p>Wee nie</p>
+//         </div>
+//         <div className="flex flex-col justify-center font-semibold text-2xl ">
+//           {renderMenuItems()}
+//         </div>
+//       </div>
+//       <div className="flex flex-col items-center mt-40 mx-10 w-1/6 h-1/3 border-4 border-gray-100 dark:border-gray-800 dark:shadow-gray-800 shadow-lg rounded-3xl">
+//         <div className="flex flex-col justify-center items-center w-60 text-2xl mt-2 h-10 font-semibold bg-white text-black dark:hover:text-white dark:text-gray-400 border-2 border-gray-100 dark:border-gray-800 dark:bg-gray-900 hover:bg-slate-100 dark:hover:bg-gray-800 dark:shadow-gray-600 shadow-lg rounded-full transition duration-300">
+//           <p>Wee nie</p>
+//         </div>
+//         <div className="flex flex-col justify-center font-semibold text-2xl ">
+//           {renderMenuItems()}
+//         </div>
+//       </div>
+//     </div>
+//     {/* <div className="flex flex-row w-full mt-14 mx-10 dark:text-gray-400 items-start font-semibold text-2xl transition duration-300">
+
+//       <div className="mt-2 w-1/2 h-3/4 items-start border-gray-100 dark:border-gray-800 border-2 rounded-2xl shadow-lg dark:shadow-gray-800"><Chart type="bar" data={data}/></div>
+
+//       <div className="mt-2 w-1/2 h-3/4 items-end border-gray-100 dark:border-gray-800 border-2 rounded-2xl shadow-lg dark:shadow-gray-800"><Chart type="bar" data={data}/></div>
+//     </div> */}
+//   </div>
+// </div>
