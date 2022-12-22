9bfa9fac0ade2ecc35e76a65f89b23ac4447dc58
The0Danktor <94552546+The0Danktor@users.noreply.github.com>
2022-12-22 13:47:59 +0100

    fuck

--- ClientApp/src/pages/TestPage.tsx
+++ ClientApp/src/pages/TestPage.tsx
@@ -7,26 +7,11 @@ export function TestPage() {
   return (
     <div className="bg-white dark:bg-gray-900 transition flex duration-300">
       <NavSide />
-      <div className="bg-white">
-        <Disclosure>
-          {({ open }) => (
-            <>
-              <Disclosure.Button>Toggle</Disclosure.Button>
-              <Transition
-                show={open}
-                as={React.Fragment}
-                enter="transition duration-100 ease-out"
-                enterFrom="transform scale-95 opacity-0"
-                enterTo="transform scale-100 opacity-100"
-                leave="transition duration-75 ease-out"
-                leaveFrom="transform scale-100 opacity-100"
-                leaveTo="transform scale-95 opacity-0"
-              >
-                <Disclosure.Panel>Panel</Disclosure.Panel>
-              </Transition>
-            </>
-          )}
-        </Disclosure>
+      <div /*center this div */ className="mx-auto flex items-center">
+        <h1>Your are:</h1>
+        <h1>With email:</h1>
+        <h1>And phone:</h1>
+        <h1>From Company:</h1>
       </div>
     </div>
   );
