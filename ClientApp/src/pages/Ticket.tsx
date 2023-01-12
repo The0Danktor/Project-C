191ea310fdae9ecfc80fc540e5a246d786aaed3b
Ant0n8 <a.shi2012@hotmail.com>
2023-01-12 15:08:42 +0100

    Update Ticket.tsx

--- ClientApp/src/pages/Ticket.tsx
+++ ClientApp/src/pages/Ticket.tsx
@@ -1,70 +1,68 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { NavSide } from "../components/Shared/NavSide";
-import { TicketLayout } from "../components/Shared/Ticket";
-import { Link } from "react-router-dom";
 import { Ticket } from "../Types/types";
+// import { AddImage } from "./addImage";
+import { Link } from "react-router-dom";
+import { TicketLayout } from "../components/Shared/Ticket";
 
-export function TicketPage() {
+interface ticketProps {
+  Id?: string
+}
+
+export function TicketPage(Id: ticketProps) {
+
+  const [tickets, setTickets] = useState<Ticket[]>();
+  
+  const getTickets = async () => {
+    (await fetch(`https://localhost:7162/api/Ticket/${Id}`))
+    .json()
+    .then(response => { 
+      setTickets(response);
+    })
+    .catch(error => {
+      console.log(error);
+    })
+  }
+
+  function Redirect() {}
   return (
     <div className="flex dark:bg-gray-900 transition duration-300">
-      <NavSide />
-      <div className="container flex flex-col flex-wrap">
-
-        <div className="w-full m-2 md:m-3">
-          <strong className="text-2xl">All Tickets</strong>
+      <div className="hidden md:flex">
+        <NavSide />
+      </div>
+      <div className="container">
+        <div className="grow w-full m-2 md:m-3">
+          <strong className="text-2xl">All tickets</strong>
           <Link to="../AddImage">
             <button
               className="border w-full md:w-40 border-gray-300 dark:border-gray-700 dark:hover:bg-gray-700 
           hover:bg-gray-200 dark:text-gray-400 rounded-lg md:ml-3 py-2"
+              onClick={Redirect}
             >
               Add Report
             </button>
           </Link>
         </div>
-        {TicketFetch().reports}
+        <>
+        {tickets !== undefined ?
+        (
+          <div>
+            {tickets.map((ticket: Ticket) => (
+              <TicketLayout
+                key={ticket.id}
+                ticket={ticket}
+              />
+            ))}
+          </div>
+        )
+        :
+        (
+          <div>
+            No Tickets
+          </div>
+        )}
+        </>
       </div>
     </div>
   );
-}
-
-export function TicketFetch() {
-  const reports = [];
-  const [_, setLoadingData] = useState<boolean>();
-  const [Tickets, setTicket] = useState<Ticket[]>();
-  const [__, setError] = useState<string>();
-
-  // loads data from database
-  const fetchData = async () => {
-    setLoadingData(true);
-    try {
-      const response = await (
-        await fetch(
-          `https://localhost:7162/api/CompanyMachine/GetByCompanyId/a7072517-250e-4582-ade2-c771d248a580`
-        )
-      ).json();
-      setTicket(response);
-    } catch (e) {
-      console.log(e);
-      setError("Unable to retrieve problems and solutions.");
-    }
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  {
-    Tickets !== undefined ? (
-      <>
-        {Tickets.map((ticket: Ticket) =>
-          reports.push(<TicketLayout ticket={ticket} />)
-        )}
-      </>
-    ) : (
-      reports.push(<p className="px-3">No tickets available</p>)
-    );
-  }
-  return (
-    {reports}
-  )
 }
