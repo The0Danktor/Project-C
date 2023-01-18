3fd456e0f8db2a92ad866b69c4427104d7826f03
The0Danktor <94552546+The0Danktor@users.noreply.github.com>
2023-01-18 22:27:19 +0100

    button names

--- ClientApp/src/pages/Departments.tsx
+++ ClientApp/src/pages/Departments.tsx
@@ -201,7 +201,7 @@ export function Departments() {
                 onChange={(e) => setName(e.target.value)}
               />
               <br />
-              <button type="submit">Create user</button>
+              <button type="submit">Create Department</button>
             </form>
           </div>
         )}
